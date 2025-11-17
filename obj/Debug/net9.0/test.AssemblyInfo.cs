13920138b1d11744ce632a7e6e0479b168dc250b
tranquanglam123tv-cyber <tranquanglam123tv@gmail.com>
2025-11-17 18:34:42 +0700

    header

--- obj/Debug/net9.0/test.AssemblyInfo.cs
+++ obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9cfaf124702d92bda7389a40e67755301a69224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0386c4c937be0f0bc4f2254e4ca62479b6f6f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
