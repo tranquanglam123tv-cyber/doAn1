5b67d08d792eb73092e11788042ca607ccdd2204
tranquanglam123tv-cyber <tranquanglam123tv@gmail.com>
2025-11-17 18:49:27 +0700

    huệ khùng

--- obj/Debug/net9.0/test.AssemblyInfo.cs
+++ obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0386c4c937be0f0bc4f2254e4ca62479b6f6f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13920138b1d11744ce632a7e6e0479b168dc250b")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
